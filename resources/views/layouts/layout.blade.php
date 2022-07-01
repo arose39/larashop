2bff1374206c20a9ddc45cacfb1b22033152569e
arose39 <andreypalamarchuk1996@gmail.com>
2022-07-01 18:37:19 +0300

    t
    .git/COMMIT_EDITMSG[+] [unix] (18:37 01/07/2022)                       102,1 Botg
    
    .git/COMMIT_EDITMSG[+] [unix] (18:37 01/07/2022)                       102,1
    
    Bot.git/COMMIT_EDITMSG[+] [unix] (18:37 01/07/2022)                       102,1 Bot

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,14 @@
+@include('includes.header')
+
+<body>
+
+
+<div class="container">
+    @include('includes.categorypannel')
+    @yield('main_content')</div>
+</body>
+
+
+@include('includes.footer')
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SiteController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [SiteController::class,'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('mainpage')->name('mainpage');
+});*/
+
+Route::get('/product', function () {
+    return view('product');
 });
